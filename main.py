11a2fa3b90ea61c3f006d4c7fb405ac8019b87f5
Abin Santy <asanty@ncsu.edu>
2023-08-11 17:10:11 -0400

    main.py

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from selenium.webdriver.common.by import By
 import time
 from getpass import getpass
 
-# Login credentials
-username = input("Username: ")
-password = getpass("Password: ")
+#Constants
+ZEBRA_PORTAL_URL = "https://portal.zebrarobotics.com/"
+ZEBRA_LMS_URL = "https://lms.zebrarobotics.com"
+
+# TODO
+# Need to learn **kwargs and *args
+def create_chrome_webdriver():
+    pass
 
 # Chrome Options
 chrome_options = webdriver.ChromeOptions()
@@ -21,23 +26,43 @@ chrome_options.add_argument("incognito")
 # Chrome WebDriver
 chrome_driver = webdriver.Chrome(options=chrome_options)
 
-# Opens the Zebra Robotics Login Page
-chrome_driver.get("https://portal.zebrarobotics.com/")
+def portal_login(driver):
+    # Portal Login Credentials
+    portal_username = input("Portal Username: ")
+    portal_password = getpass("Portal Password: ")
 
-# Waiting for login page to load
-chrome_driver.implicitly_wait(5)
+    # Opens the Zebra Robotics Login Page
+    driver.get(ZEBRA_PORTAL_URL)
 
-# Enters username and password, then clicks submit button
-chrome_driver.find_element(By.NAME, 'email').send_keys(username)
-chrome_driver.find_element(By.NAME, 'password').send_keys(password)
-chrome_driver.find_element(By.ID, 'kt_login_signin_submit').click()
+    # Waiting for login page to load
+    driver.implicitly_wait(5)
 
-# Selects the branch
-chrome_driver.find_element(By.ID, 'kt_subheader_quick_actions').click()
-chrome_driver.find_element(By.LINK_TEXT, 'Cary').click()
+    # Enters username and password, then clicks submit button
+    driver.find_element(By.NAME, 'email').send_keys(portal_username)
+    driver.find_element(By.NAME, 'password').send_keys(portal_password)
+    driver.find_element(By.ID, 'kt_login_signin_submit').click()
+
+    # Selects the branch
+    driver.find_element(By.ID, 'kt_subheader_quick_actions').click()
+    driver.find_element(By.LINK_TEXT, 'Cary').click()
+    # Saves the window ID for ZR portal
+    portal_window_id = chrome_driver.current_window_handle
+
+def canvas_login(driver):
+    # Creates a new tab and navigates to Canvas
+    driver.switch_to.new_window('tab')
+    driver.get(ZEBRA_LMS_URL)
+
+    # LMS Login Credentials
+    lms_username = input("LMS Username: ")
+    lms_password = getpass("LMS Password: ")
+
+    # Enters username and password into LMS, then clicks submit
+    driver.find_element(By.ID, 'pseudonym_session_unique_id').send_keys(lms_username)
+    driver.find_element(By.ID, 'pseudonym_session_password').send_keys(lms_password)
 
 # Minimizes the window until further input
-chrome_driver.minimize_window()
+#chrome_driver.minimize_window()
 
 ### TEMPORARY
 # Keeps window open until user exits with Ctrl+C
@@ -49,3 +74,6 @@ except KeyboardInterrupt:
 
 # Closes the WebDriver manually
 chrome_driver.quit()
+
+if __name__ == '__main__':
+    pass
